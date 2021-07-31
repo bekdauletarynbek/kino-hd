a18ae85c446a694f69e0927ca83aca9db1950d0d
bekda_000 <bekdauletarynbek@gmail.con>
2021-07-31 13:36:58 +0600

    some changes

--- src/components/tv.vue
+++ src/components/tv.vue
@@ -9,15 +9,20 @@
 </template>
 
 <script>
-import movieStatus from "@/services/movieStatus";
 export default {
   name: "movie",
-  mixins: [movieStatus],
   props: {
     tv: Object,
     id: Number
   },
   methods: {
+    movieStatus(vote) {
+      let str = 'p-1 rounded text-gray-100'
+      if (vote >= 8.0) return str+' bg-green-600'
+      if (vote >= 6.0 && vote < 8.0) return str+ ' bg-yellow-600'
+      if (vote >= 5.0 && vote < 6.0) return str+' bg-gray-600'
+      if (vote < 5.0) return str+' bg-red-600'
+    }
   }
 }
 </script>
--- src/views/movie.vue
+++ src/views/movie.vue
@@ -91,14 +91,12 @@
 </template>
 
 <script>
-import movieStatus from "@/services/movieStatus";
 import YoutubeVue from "../components/YoutubeVue";
 import navItemMovie from '../components/custom-ui/nav-item-movie'
 import Details from "../components/details";
 
 export default {
   name: "movie",
-  mixins: [movieStatus],
   components: {
     YoutubeVue,
     navItemMovie,
@@ -166,6 +164,13 @@ export default {
     },
     changeDetail() {
       console.log(1)
+    },
+    movieStatus(vote) {
+      let str = 'p-1 rounded text-gray-100'
+      if (vote >= 8.0) return str+' bg-green-600'
+      if (vote >= 6.0 && vote < 8.0) return str+ ' bg-yellow-600'
+      if (vote >= 5.0 && vote < 6.0) return str+' bg-gray-600'
+      if (vote < 5.0) return str+' bg-red-600'
     }
   }
 }
