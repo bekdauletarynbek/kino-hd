bc106e94a91bed39973e6de88eedd1fec859f374
bekdaulet <bekdauletarynbek@gmail.com>
2021-09-27 16:14:27 +0600

    some

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f bekdauletarynbekgit@github.com/kino-hd main:gh-pages
+git push -f bekdauletarynbek@github.com/kino-hd main:gh-pages
 
 cd -
